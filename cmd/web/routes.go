85c4379e56bbddd58983b6a132f3850adc4dfd62
Danang Ari Murti Wibowo <61486311+danangamw@users.noreply.github.com>
2024-11-27 13:38:44 +0700

    add: about page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() http.Handler {
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /about", dynamic.ThenFunc(app.about))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 
 	// auth routes
